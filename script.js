b18dfcbf86d963151ff3ce2218c2a17c4f9604d8
Jérôme Dumas <jerome.dumas@gmail.com>
2017-06-12 12:35:54 +0200

    Intégration de la section votre visite + mise en forme suite échange Eric

---
+++ script.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAJKRLAGc_LF1ILyY6ilEVmnpAZR9CpCIA",
+  authDomain: "locomiam-shipping-offers.firebaseapp.com",
+  databaseURL: "https://locomiam-shipping-offers.firebaseio.com",
+  projectId: "locomiam-shipping-offers",
+  storageBucket: "locomiam-shipping-offers.appspot.com",
+  messagingSenderId: "1041222257094"
+};
+firebase.initializeApp(config);
+
